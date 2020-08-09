5643801d4ae968a12c67e22aa473fea9de9d93ff
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-08-09 19:39:18 -0300

    Finally back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 
 import ClassController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classControllers = new ClassController();
+const connectionController = new ConnectionController();
 
 routes.post('/classes', classControllers.create);
 routes.get('/classes', classControllers.index);
+
+routes.post('/connections', connectionController.create)
+routes.get('/connections', connectionController.index)
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+app.use(cors);
 
 const port = process.env.port || 3333;
 app.listen(port);
