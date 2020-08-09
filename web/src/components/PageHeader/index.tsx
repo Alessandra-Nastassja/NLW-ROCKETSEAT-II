32871b75ee6bfe64bc75e1c63e4195d8f193bf6f
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-08-09 20:44:11 -0300

    Input component

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import backIcon from '../../assets/icons/back.svg';
-import logoImg from '../../assets/logo.svg';
+import backIcon from "../../assets/icons/back.svg";
+import logoImg from "../../assets/logo.svg";
 
-import './styles.css';
+import "./styles.css";
 
 interface PageHeaderProps {
-    title: string;
+  title: string;
+  description?: string;
 }
 
-const  PageHeader: React.FC<PageHeaderProps> = (props) => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -23,10 +24,12 @@ const  PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
+
         {props.children}
       </div>
     </header>
   );
-}
+};
 
 export default PageHeader;
