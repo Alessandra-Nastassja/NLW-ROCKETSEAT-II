dffaa326d5ebaf310e14cb7cd9df9ac259cb2d5a
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-08-10 22:47:18 -0300

    Adjustment select

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,9 +5,9 @@ import PageHeader from "../../components/PageHeader";
 import Textarea from "../../components/Textarea";
 import Select from "../../components/Select";
 
-import warningIcon from '../../assets/icons/warning.svg'
+import warningIcon from "../../assets/icons/warning.svg";
 
-import './styles.css';
+import "./styles.css";
 
 function TeacherForm() {
   return (
@@ -21,43 +21,67 @@ function TeacherForm() {
         <fieldset>
           <legend>Seus dados</legend>
 
-          <Input name="name" label="Nome completo"/>
-          <Input name="avatar" label="Avatar"/>
-          <Input name="whatsapp" label="Whatsapp"/>
-          <Textarea name="bio" label="Biografia"/>
-
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Select 
-            name="subject" 
+          <Select
+            name="subject"
             label="Matéria"
             options={[
-              {value: 'Artes', label: 'Artes'},
-              {value: 'Biologia', label: 'Biologia'},
-              {value: 'Ciências', label: 'Ciências'},
-              {value: 'Educação física', label: 'Educação física'},
-              {value: 'Geografia', label: 'Geografia'},
-              {value: 'História', label: 'História'},
-              {value: 'Matemática', label: 'Matemática'},
-              {value: 'Português', label: 'Português'},
-              {value: 'Química', label: 'Química'},
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação física", label: "Educação física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
             ]}
           />
-          <Input name="cost" label="Custo da sua hora por aula"/>
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">
+              + Novo horário
+            </button>
+            </legend>
+
+            <div className="schedule-item">
+              <Select
+                name="week_day" 
+                label="Dia da semana"
+                options={[
+                  {value: '0', label: 'Domingo'},
+                  {value: '1', label: 'Segunda-feira'},
+                  {value: '2', label: 'Terça-feira'},
+                  {value: '3', label: 'Quarta-feira'},
+                  {value: '4', label: 'Quinta-feira'},
+                  {value: '5', label: 'Sexta-feira'},
+                  {value: '6', label: 'Sábado'},
+                ]}
+              />
+              <Input name="from" label="Das" type="time"></Input>
+              <Input name="to" label="Até" type="time"></Input>
+            </div>
         </fieldset>
 
         <footer>
           <p>
-            <img src={warningIcon} alt="Aviso importante"/>
-            Importante! <br/>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
             Preencha todos os dados
           </p>
-          <button type="button">
-            Salvar cadastro
-          </button>
+          <button type="button">Salvar cadastro</button>
         </footer>
       </main>
     </div>
